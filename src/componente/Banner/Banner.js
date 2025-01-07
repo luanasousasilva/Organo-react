b757b5df850e2cd5d9413b7c323995cf6be8f2a4
luana.silva <luana.silva@bitgov.com.br>
2025-01-06 21:16:10 -0300

    Criação do componente banner e a sua estilização.

---
+++ src/componente/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX  e como o react ler e transforma em elementos don //
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt=" Banner principal da pagina organo"/>
+        </header>
+    )
+}
+
+export default Banner
