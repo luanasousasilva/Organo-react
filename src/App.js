081a4289f04f1f3bf3d5ae0881a164babf4ec773
luana.silva <luana.silva@bitgov.com.br>
2025-01-08 21:12:26 -0300

    Criação do componente do texto e do formulario.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Banner from "./componente/Banner/Banner";
+import Formulario from "./componente/Formulario";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Banner/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Banner/>
+            <Formulario/>
+        </div>
+    );
 }
 
 export default App;
